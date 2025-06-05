c7feff78f1c5b2806291bbdadf98b6621a7b24f9
evandrollima77 <evandrolima2377@gmail.com>
2025-06-05 11:33:11 -0300

    vi como usar o for

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,7 +1,8 @@
 /*let number1= 30;
 let number2= 20;
 let som = number1+number2
-console.log(" a soma é "+som)
+console.
+log(" a soma é "+som)
 console.log("-----------------")
 let number3= 10;
 let number4= 5;
@@ -17,10 +18,109 @@ let number7= 4;
 let number8= 2;
 let divisão = number7/number8
 console.log(" a divisão é igual : " +divisão)*/
-let nome = "josé";
+/*let nome = "josé";
 let salario = 1518;
 let mestrab = 12;
 let mtb = 6;
 let decimo = salario/mestrab*mtb
+console.log( "parabéns josé seu decimo é :R$ " +decimo+ " parabéns " )*/
+/*
+let nome = prompt(" digite seu nome : ")
+let salario = prompt("digite seu salario: ")
+let mesestrab = prompt(" digite os meses que vc trabalhou: ")
+let decim = salario/12*mesestrab
+alert (nome+" seu décimo 13 é: " + decim + " R$")*/
 
-console.log( "parabéns josé seu decimo é :R$ " +decimo+ " parabéns " )
+/*let idade = prompt(" digite sua idade: ")
+if(idade<12) {
+alert(" assista conteudo infantil ")
+}
+else if(idade>=12 && idade<=17){
+    alert(" assista conteudo teen ")
+}
+else if(idade>=18 && idade<=60){
+    alert(" assista conteudo adulto ")
+}
+else{
+    alert(" assista conteudo senior ")
+}*/
+
+/*let pontuacao = prompt(" digite sua pontuação: ")
+if(pontuacao<=1000) {
+    alert("parabéns novato seu bonus de xp é 100xp!!")
+}
+else if(pontuacao>=1001 && pontuacao<=5000){
+    alert("parabéns aprendiz seu bonus de xp e 500xp!!")
+}*/
+
+/*let basico = 25;
+let premium = 40;
+let ultra = 60;
+let tela34 = 5;
+let telamaisde4 = 7.50;
+
+let assinatura = prompt("digite sua assinatura: ")
+let telas = prompt(" quantas telas você gostaria ?")
+if(assinatura=="basico") {
+   if(telas >=1 && telas  <=2){
+    alert(" seu plano terá o valor de R$ "+basico)
+ }
+}
+else if(telas >=3 && telas <=4){
+alert(" seu plano terá o valor de R$ "+(basico+tela34))
+}
+else {
+    
+}*/
+/*let daysOFweek = ["zeca", "junior"];
+console.log(daysOFweek[1])
+console.log(daysOFweek.length)
+let listdiver = ["caio",1,2.5,"escola",false]
+console.log(listdiver[3])
+let nomedoaluno = prompt("digite o nome do novo aluno: ")
+daysOFweek.push("tony, kaique")
+daysOFweek.push(nomedoaluno)
+console.log("os alunos da escola são:" +daysOFweek)*/
+
+/*let daysOFweek = ["caio", "samuel",0,70];
+console.log(daysOFweek[1])
+console.log(daysOFweek.length)
+let listdiver = ["caio",1,2.5,"escola",false]
+console.log(listdiver[3])
+let nomedoaluno = prompt("digite o nome do novo aluno: ")
+daysOFweek.push(nomedoaluno)
+daysOFweek.unshift("richard")
+daysOFweek.unshift("kaique","tony","zeca")
+daysOFweek.shift()
+daysOFweek.sort()
+daysOFweek.reverse()
+console.log("os alunos da escola são:" +daysOFweek)
+console.log(daysOFweek.indexOf ("samuel"))*/
+/*var array = ["valor1", "valor2" , "valor3", "valor4"]
+for (let i = 0; i < array.length; i++){
+    console.log(i);
+}
+const alunos = ["ana", "bruno", "carlos", "diana"];
+for(const aluno of alunos){
+    console.log("olá, " +aluno+ "!");
+}*/
+/*let number = Number(prompt("digite o número que deseja somar aos anteriores: "))
+let numberfinal = 0;
+while (number != 0){
+      numberfinal = numberfinal+number
+      number = Number(prompt("digite o número que deseja somar aos anteriores: "))
+}
+console.log("o número final é: " +numberfinal)*/
+
+/*let senhacorreta = "evandro123";
+let senhadigitada = prompt("digite sua senha: ")
+
+while (senhacorreta !== senhadigitada){
+    console.log("incorreta, digite a senha novamente. ");
+    senhadigitada = prompt("digite sua senha: ");
+    console.log("bem vindo ao sistema!")
+}*/
+
+for (let i = 1; i <11; i++){
+    console.log(i);
+}
